7bf1e27ed1fb9c42f2897d38ba13cb0efeac1938
juanchete <juanchete@gmail.com>
2020-04-17 18:13:39 +0200

    hope it triggers

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import jwt
 from flask import Flask, jsonify, request, abort
 
 #simple comment to trigger the pipeline ;)
-
+#Another comment to trigger the pipeline, cause the first did not work
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
