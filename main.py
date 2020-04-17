df4f2c1c750f1b4d7a2b3414c09f99a30995d3e6
juanchete <juanchete@gmail.com>
2020-04-17 18:30:32 +0200

    third try

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from flask import Flask, jsonify, request, abort
 
 #simple comment to trigger the pipeline ;)
 #Another comment to trigger the pipeline, cause the first did not work
+#Another comment to trigger the pipeline, cause the second did not work - now with a access_token 
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
